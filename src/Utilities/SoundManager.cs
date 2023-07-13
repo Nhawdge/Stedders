2464dd0e3cdf624234cc2f6cefc433c02ba32aee
John Pavek <JohnQPavek@gmail.com>
2023-07-13 09:48:46 -0500

    Working on mac build

---
+++ src/Utilities/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_CsLo;
+
+namespace Stedders.Utilities
+{
+    internal class SoundManager
+    {
+        public Dictionary<SoundKey, Texture> SoundStore { get; set; } = new();
+
+        public void LoadTextures()
+        {
+            SoundStore.Add(SoundKey.Ambiance, Raylib.LoadTexture("Assets/Sound/Stedders_Day_Map_Ambience.wav"));
+            SoundStore.Add(SoundKey.FlowerGrowth, Raylib.LoadTexture("Assets/Sound/Stedders_Flower_Growing.wav"));
+        }
+        public Texture GetTexture(SoundKey key)
+        {
+            return SoundStore[key];
+        }
+    }
+
+    public enum SoundKey
+    {
+        Ambiance,
+        FlowerGrowth,
+    }
+}
